088075f71d56bdfe0bea48c99b18e7817a6b03a6
Jafaros <Grapetr@seznam.cz>
2025-12-13 11:37:15 +0100

    Vytvoření klienta a nastavení proxy na API

---
+++ Grajciar.InternetBanking.Client/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { APP_TITLE } from '$lib/config';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>{APP_TITLE}</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+{@render children()}
